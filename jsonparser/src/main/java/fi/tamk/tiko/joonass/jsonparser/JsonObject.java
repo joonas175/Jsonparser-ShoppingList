2dd805134931a33cf7872951f376d4c979fc4a23
Unknown <joonas.salojarvi@cs.tamk.fi>
2018-11-11 00:14:12 +0200

    Inital commit
    Empty mvn project with 2 modules

---
+++ jsonparser/src/main/java/fi/tamk/tiko/joonass/jsonparser/JsonObject.java
@@ -0,0 +1,5 @@
+package fi.tamk.tiko.joonass.jsonparser;
+
+public class JsonObject {
+    
+}
---
+++ shoppinglist-gui/src/main/java/fi/tamk/tiko/joonass/App.java
@@ -0,0 +1,17 @@
+package fi.tamk.tiko.joonass;
+
+import fi.tamk.tiko.joonass.jsonparser.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    
+    }
+
+}
