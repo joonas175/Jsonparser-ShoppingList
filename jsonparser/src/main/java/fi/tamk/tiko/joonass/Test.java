9139027e7ef82b91b2b64b8fd74226cf1586a992
Unknown <joonas.salojarvi@cs.tamk.fi>
2018-11-20 21:02:37 +0200

    Tiedostonimi vaihtunut

--- jsonparser/src/main/java/fi/tamk/tiko/joonass/Test.java
+++ jsonparser/src/main/java/fi/tamk/tiko/joonass/Test.java
@@ -6,34 +6,31 @@ import fi.tamk.tiko.joonass.jsonparser.JsonObject;
 import fi.tamk.tiko.joonass.jsonparser.JsonStringBuilder;
 
 /**
- * Hello world!
- *
+ * 
  */
-public class App 
+public class Test
 {
     public static void main( String[] args )
     {
+        System.out.println("Author: Joonas Salojarvi");
         JsonObject obj = new JsonObject();
         obj.put("Name", "Jaska");
         obj.put("Ikä", 6);
-        obj.put("Kikkelinpituus", 0.2);
+        obj.put("Randomluku", 0.2);
         obj.put("Mies", true);
         JsonObject obj2 = new JsonObject();
-        obj2.put("Name", "Jaska");
+        obj2.put("Name", "JokuToinen");
         obj2.put("Ikä", 6);
-        obj2.put("Kikkelinpituus", 0.2);
+        obj2.put("Sormenpaksuus", 0.2);
         JsonObject obj3 = new JsonObject();
-        obj3.put("Name", "Jaska");
-        obj3.put("Ikä", 6);
+        obj3.put("Name", "Teukka");
+        obj3.put("Ikä", 54);
         obj3.put("Kikkelinpituus", 0.2);
         obj2.put("kolmasjaska", obj3);
         obj.put("ToinenJaska", obj2);
         obj.put("Nulliarvo", null);
         JsonArray array = new JsonArray("asd", 1, 5, 1, true);
         obj.putArray("Taulukko", "asd", array);
-        //System.out.println(JsonStringBuilder.build(obj));
-        System.out.println(obj.toString());
-        System.out.println(JsonStringBuilder.build(obj));
         JsonFileWriter writer = new JsonFileWriter("test.txt");
         writer.write(obj,false);
     }
