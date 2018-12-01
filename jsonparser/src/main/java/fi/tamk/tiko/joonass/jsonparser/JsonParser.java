bd5672bf4f51e7beeb41188bb913927b4e99951b
Unknown <joonas.salojarvi@cs.tamk.fi>
2018-12-01 22:16:44 +0200

    fresh start

--- jsonparser/src/main/java/fi/tamk/tiko/joonass/jsonparser/JsonParser.java
+++ jsonparser/src/main/java/fi/tamk/tiko/joonass/jsonparser/JsonParser.java
@@ -2,16 +2,12 @@ package fi.tamk.tiko.joonass.jsonparser;
 
 public class JsonParser {
 
-    public static int iterator;
+    public int iterator;
 
-    public JsonElement parseObjectFromString(String source) {
+    public JsonObject parseObjectFromString(String source) {
         source = source.trim().replace("\n", "");
 
-        for(int i = 0; i < source.length(); i++){
-            if(source.charAt(i) == )
-        }
-
-
-        return parentJsonObj;
+        return new JsonObject();
     }
+
 }
